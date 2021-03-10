48c22e946e990619b3491297c99d4ca38e11dc53
phanhoangha96 <phanhoangha96@gmail.com>
2021-03-10 10:30:29 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ljdvtg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ljdvtg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular 6";
+  keyType: any = "Numeric";
+  task: {
+    [id: 1name: "AngularJS"]: ;
+    [id: 2name: "ReactJS"]: ;
+    [id: 2name: "VueJS"]: ;
+    [id: 3name: "NodeJS"]: ;
+  };
+}
